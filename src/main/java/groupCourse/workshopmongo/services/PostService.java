5fbabb48277dc8f0872597adb6ef977c4462c61b
Matheus Ribeiro <Nogahzera@gmail.com>
2022-11-23 01:30:04 -0300

    obtaining posts with an ID

---
+++ src/main/java/groupCourse/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package groupCourse.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import groupCourse.workshopmongo.domain.Post;
+import groupCourse.workshopmongo.repository.PostRepository;
+import groupCourse.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
